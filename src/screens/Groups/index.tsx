bcc169b0ace1deb47662940902f7155c16249b8f
douglasbertelli <73892745+douglasbertelli@users.noreply.github.com>
2024-10-03 18:47:35 -0300

    Novo: GroupCard é um componente que é TouchableOpacity com Icon e Texto.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,12 +4,15 @@ import {Container} from './styles';
 
 import Header from '@components/Header';
 import Highlight from '@components/Highlight';
+import GroupCard from "@components/GroupCard";
 
 export default function Groups() {
 	return (
 		<Container>
 			<Header/>
 			<Highlight title={"Olá..."} subtitle={"Lorem Ipsum"}/>
+			<GroupCard title={'Componente de grupo.'} onPress={() => {
+			}}/>
 		</Container>
 	);
 }
