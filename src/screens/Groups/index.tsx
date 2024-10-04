d8077c1a03c16140e3d83e7f37f566649a4dd06d
douglasbertelli <73892745+douglasbertelli@users.noreply.github.com>
2024-10-04 11:12:35 -0300

    1. Componente de lista (Flatlist)
    2. ListEmpty componente para o Flatlist que mostra mensagem caso não existam itens na lista.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {Container} from './styles';
 
 import Header from '@components/Header';
 import Highlight from '@components/Highlight';
 import GroupCard from "@components/GroupCard";
+import {FlatList, StyleSheet} from "react-native";
+import ListEmpty from "@components/ListEmpty";
+
 
 export default function Groups() {
+	const [groups, setGroups] = useState<string[]>(['NodeJS', 'PHP', 'JavaScript', 'React Native', 'Flutter', 'Kotlin', 'Java', 'Go', 'Python']);
+
 	return (
 		<Container>
 			<Header/>
-			<Highlight title={"Olá..."} subtitle={"Lorem Ipsum"}/>
-			<GroupCard title={'Componente de grupo.'} onPress={() => {
-			}}/>
+			<Highlight title={"Turmas"} subtitle={"Jogue com a sua turma"}/>
+
+			<FlatList
+				style={styles.List}
+				data={groups}
+				keyExtractor={item => item}
+				contentContainerStyle={groups.length === 0 && {flex: 1}}
+				renderItem={({item}) => (
+					<GroupCard
+						title={item}
+					/>
+				)}
+				ListEmptyComponent={() => <ListEmpty message={"Que tal cadastrar a primeira turma?"}/>}
+			/>
+
 		</Container>
 	);
 }
+
+const styles = StyleSheet.create({
+	List: {
+		width: '100%',
+	}
+})
