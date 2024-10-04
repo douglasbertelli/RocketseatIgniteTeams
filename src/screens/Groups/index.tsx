3913f785a20be59c228b96c634d172294702dfca
douglasbertelli <73892745+douglasbertelli@users.noreply.github.com>
2024-10-04 20:41:53 -0300

    1. Fazendo a tipagem da routas.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,24 @@
 import React, {useState} from 'react';
 
 import {Container} from './styles';
+import {FlatList, StyleSheet} from "react-native";
+import {useNavigation} from "@react-navigation/native";
 
 import Header from '@components/Header';
 import Highlight from '@components/Highlight';
 import GroupCard from "@components/GroupCard";
-import {FlatList, StyleSheet} from "react-native";
 import ListEmpty from "@components/ListEmpty";
 import Button from "@components/Button";
 
 
 export default function Groups() {
-	const [groups, setGroups] = useState<string[]>(['NodeJS', 'PHP', 'JavaScript', 'React Native', 'Flutter', 'Kotlin', 'Java', 'Go', 'Python']);
+	const [groups, setGroups] = useState(['NodeJS', 'PHP', 'JavaScript', 'React Native', 'Flutter', 'Kotlin', 'Java', 'Go', 'Python']);
+
+	const navigation = useNavigation()
+
+	function handleNewGroup() {
+		navigation.navigate("new");
+	}
 
 	return (
 		<Container>
@@ -31,7 +38,10 @@ export default function Groups() {
 				ListEmptyComponent={() => <ListEmpty message={"Que tal cadastrar a primeira turma?"}/>}
 			/>
 
-			<Button title={'Criar nova turma'}/>
+			<Button
+				title={'Criar nova turma'}
+				onPress={handleNewGroup}
+			/>
 
 		</Container>
 	);
